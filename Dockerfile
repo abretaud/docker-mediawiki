ce60e9138ab61b490676a603c8dc16f0ebbe661d
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2017-04-27 16:13:09 +0200

    first commit

---
+++ Dockerfile
@@ -0,0 +1,36 @@
+FROM kristophjunge/mediawiki:1.28
+MAINTAINER Anthony Bretaudeau <anthony.bretaudeau@inra.fr>
+
+# Install psql php ext
+RUN apt-get -q update && \
+    DEBIAN_FRONTEND=noninteractive apt-get -yq --no-install-recommends install \
+    nano libpq-dev postgresql-client && \
+    BUILD_DEPS="libpq-dev"; \
+    DEBIAN_FRONTEND=noninteractive apt-get -yq --no-install-recommends install $BUILD_DEPS \
+ && docker-php-ext-install pgsql pdo_pgsql \
+ && apt-get purge -y --auto-remove -o APT::AutoRemove::RecommendsImportant=false -o APT::AutoRemove::SuggestsImportant=false $BUILD_DEPS \
+ && rm -rf /var/lib/apt/lists/*
+
+# Add a newer mediawiki version
+ARG MEDIAWIKI_VERSION_MAJOR=1.28
+ARG MEDIAWIKI_VERSION=1.28.1
+ADD https://releases.wikimedia.org/mediawiki/$MEDIAWIKI_VERSION_MAJOR/mediawiki-$MEDIAWIKI_VERSION.tar.gz /tmp/mediawiki.tar.gz
+RUN rm -rf /var/www/mediawiki && \
+    mkdir -p /var/www/mediawiki /data /images && \
+    tar -xzf /tmp/mediawiki.tar.gz -C /tmp && \
+    mv /tmp/mediawiki-$MEDIAWIKI_VERSION/* /var/www/mediawiki && \
+    rm -rf /tmp/mediawiki.tar.gz /tmp/mediawiki-$MEDIAWIKI_VERSION/ && \
+    chown -R www-data:www-data /data /images && \
+    rm -rf /var/www/mediawiki/images && \
+    ln -s /images /var/www/mediawiki/images
+COPY config/mediawiki/* /var/www/mediawiki/
+
+# REMOET_USER auth extension
+ARG EXTENSION_REMOTEUSER_VERSION=master-c316e11
+ADD https://extdist.wmflabs.org/dist/extensions/Auth_remoteuser-$EXTENSION_REMOTEUSER_VERSION.tar.gz /tmp/extension-remoteuser.tar.gz
+RUN tar -xzf /tmp/extension-remoteuser.tar.gz -C /var/www/mediawiki/extensions && \
+rm /tmp/extension-remoteuser.tar.gz
+
+COPY config/nginx/* /etc/nginx/
+COPY config/mediawiki/* /var/www/mediawiki/
+COPY script/* /script/
---
+++ script/docker-entry.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# Fix permissions of images folder
+chown -R 999:999 /var/www/mediawiki/images
+
+# Set upload size default to be used in PHP config
+MEDIAWIKI_MAX_UPLOAD_SIZE=${MEDIAWIKI_MAX_UPLOAD_SIZE:="100M"}
+export MEDIAWIKI_MAX_UPLOAD_SIZE
+
+# Setup nginx configs
+MEDIAWIKI_HTTPS=${MEDIAWIKI_HTTPS:=0}
+MEDIAWIKI_SMTP_SSL_VERIFY_PEER=${MEDIAWIKI_SMTP_SSL_VERIFY_PEER:=0}
+
+if [ ${MEDIAWIKI_HTTPS} == 1 ]; then
+    # Use HTTPS config
+    mv /etc/nginx/nginx-https.conf /etc/nginx/nginx.conf
+else
+    # Use HTTP config
+    mv /etc/nginx/nginx-http.conf /etc/nginx/nginx.conf
+fi
+
+# Disable SSL peer verification in PEAR mail class to support self signed certs
+if [ ${MEDIAWIKI_SMTP_SSL_VERIFY_PEER} == 0 ]; then
+    sed -i "s/if (isset(\$params\['socket_options'\])) \$this->socket_options = \$params\['socket_options'\];/if (isset(\$params['socket_options'])) \$this->socket_options = \$params['socket_options'];\\n\$this->socket_options['ssl']['verify_peer'] = false;\\n\$this->socket_options['ssl']['verify_peer_name'] = false;/g" /usr/local/lib/php/Mail/smtp.php
+fi
+
+echo
+echo "=> Trying to connect to a database using:"
+echo "      Database Driver:   $MEDIAWIKI_DB_TYPE"
+echo "      Database Host:     $MEDIAWIKI_DB_HOST"
+echo "      Database Port:     $MEDIAWIKI_DB_PORT"
+echo "      Database Username: $MEDIAWIKI_DB_USER"
+echo "      Database Password: $MEDIAWIKI_DB_PASSWORD"
+echo "      Database Name:     $MEDIAWIKI_DB_NAME"
+echo
+
+for ((i=0;i<20;i++))
+do
+    DB_CONNECTABLE=$(PGPASSWORD=$MEDIAWIKI_DB_PASSWORD psql -U "$MEDIAWIKI_DB_USER" -h "$MEDIAWIKI_DB_HOST" -p "$MEDIAWIKI_DB_PORT" -l >/dev/null 2>&1; echo "$?")
+	if [[ $DB_CONNECTABLE -eq 0 ]]; then
+		break
+	fi
+    sleep 3
+done
+
+if ! [[ $DB_CONNECTABLE -eq 0 ]]; then
+	echo "Cannot connect to database"
+    exit "${DB_CONNECTABLE}"
+fi
+
+### Initial setup if database doesn't exist
+if [ "$(PGPASSWORD=$MEDIAWIKI_DB_PASSWORD psql -U "$MEDIAWIKI_DB_USER" -h "$MEDIAWIKI_DB_HOST" -p "$MEDIAWIKI_DB_PORT" -tAc "SELECT 1 FROM pg_database WHERE datname='$MEDIAWIKI_DB_NAME'" )" != '1' ]
+then
+    echo "Database $MEDIAWIKI_DB_NAME does not exist, creating it"
+    echo "CREATE DATABASE $MEDIAWIKI_DB_NAME;" | psql -U "$MEDIAWIKI_DB_USER" -h "$MEDIAWIKI_DB_HOST" -p "$MEDIAWIKI_DB_PORT" postgres;
+fi
+
+# Check if tables are there and that drush works
+DB_LOADED=$(PGPASSWORD=$MEDIAWIKI_DB_PASSWORD psql -U "$MEDIAWIKI_DB_USER" -h "$MEDIAWIKI_DB_HOST" -p "$MEDIAWIKI_DB_PORT" -tAc "SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'page');")
+if [[ $DB_LOADED != "t" ]]
+then
+	echo "Installing db schema..."
+    /script/install.sh ${MEDIAWIKI_ADMIN_USER:=admin} ${MEDIAWIKI_ADMIN_PASSWORD:=admin}
+fi
+
+# Start supervisord
+/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
